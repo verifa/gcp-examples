386e27f6e09a53b4abd6bc4faab4eb36f80316c9
kabhigita <akalaiyarasan@verifa.io>
2023-12-14 09:05:30 +0200

    modify mysite, app.py

--- app-engine/app.py
+++ app-engine/app.py
@@ -1,10 +1,21 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello_world():
-        return "<p>Hello, World!</p>"
+def hello():
+	return """<h1>Hello People</h1>
+			  <br>
+			  <body> your lucky number is """ + str(luckynumber()) + """</body>"""
+
+@app.route("/moi")
+def moi():
+	return "<h2>Moi!</h2>"
+
+def luckynumber():
+	return random.randint(0,100)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+	app.run(host='0.0.0.0')
